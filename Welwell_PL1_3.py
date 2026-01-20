4d8c133b7a9f304febbe673a0e28406460f2f665
NiceGoodtimes <elwell.wi@northeastern.edu>
2026-01-19 23:51:27 -0500

    Final form of the reverser program, now following instructions to return square roots of both original and reversed numbers

--- Welwell_PL1_3.py
+++ Welwell_PL1_3.py
@@ -2,8 +2,15 @@
 original = str(input("Enter a positive, five-digit number: "))
 
 #functions
-def reverser():
-    return original[::-1]
-
+def reverser(a):
+    return a[::-1]
+def square_rooter(a):
+    x = float(a)
+    return x**0.5
 #main
-print(reverser())
+part_1 = f"The square root of {original} is: "
+part_3 = f"The square root of {reverser(original)} is: "
+
+print(part_1, square_rooter(original))
+print("The number in reverse is:    ", reverser(original))
+print(part_3, square_rooter(reverser(original)))
