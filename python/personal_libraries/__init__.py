d89518265d8d61ee87a2d985899753fc8c61914a
NiceGoodtimes <elwell.wi@northeastern.edu>
2026-02-01 21:11:51 -0500

    Setting up greater potential for expandable library

---
+++ python/personal_libraries/__init__.py
@@ -0,0 +1,6 @@
+from .numbers import is_even, is_odd
+
+__all__ = ["is_even", "is_odd"]
+
+#version
+__version__ = "0.1.0"
--- python/personal_libraries/numbers.py
+++ python/personal_libraries/numbers.py
@@ -1,12 +1,15 @@
+#setup
+from typing import Optional
+
 #even checker
-def is_even(n):
+def is_even(n: int) -> bool:
     if n % 2 == 0:
         return True
     else:
         return False
 
 #odd checker
-def is_odd(n):
+def is_odd(n: int) -> bool:
     if n % 2 == 1:
         return True
     else:
