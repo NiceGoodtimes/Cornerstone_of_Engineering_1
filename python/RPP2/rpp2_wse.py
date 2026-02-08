8b44408ce5347417b696ab312505fe87fc3a4e4f
welwell <elwell.wi@northeastern.edu>
2026-02-08 15:58:21 -0500

    Simple program for infared sensor

---
+++ python/RPP2/rpp2_wse.py
@@ -0,0 +1,16 @@
+#imports
+from machine import Pin
+import time
+
+#pins
+led = Pin(14, Pin.OUT)
+sensor = Pin(15, Pin.IN)
+
+#main
+while True:
+    print(sensor.value())
+    time.sleep(0.1)
+    if sensor.value() == 0:
+        led.value(1)
+    else:
+        led.value(0)
