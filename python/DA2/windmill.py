c1f63fe5465ca758f1274eaba4ed85060cec68f1
welwell <elwell.wi@northeastern.edu>
2026-02-11 15:59:09 -0500

    small updates 1

--- python/DA2/windmill.py
+++ python/DA2/windmill.py
@@ -3,9 +3,17 @@ from machine import Pin
 import time
 
 #variables
+seconds = 0
 n_cycle = 0
+n_blades = 3
 
 #functions
+"""
+def clock(seconds):
+    time.sleep(1)
+    seconds += 1
+    return seconds"""
+
 def changed(x, n):
     n = n%2
     values = []
@@ -16,6 +24,12 @@ def changed(x, n):
     else:
         return False
 
+def rpm(x):
+    blades_seen = 0
+    if changed(infared_sensor.value(), n_cycle):
+        blades_seen += 1
+
+
 #pins
 output = Pin(14, Pin.OUT)
 infared_sensor = Pin(15, Pin.IN)
@@ -25,9 +39,7 @@ while True:
     n_cycle =+ 1
 
     print(infared_sensor.value())
-    time.sleep(0.01)
     if infared_sensor.value() == 0:
         output.value(1)
     else:
         output.value(0)
-
