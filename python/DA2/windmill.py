0c83b0ba8f20e2b28f2a1b57e3de0612399b8beb
welwell <elwell.wi@northeastern.edu>
2026-02-15 21:07:08 -0500

    Bug fixes pt. 2 - corrected changed and change_counter functions

--- python/DA2/windmill.py
+++ python/DA2/windmill.py
@@ -1,5 +1,5 @@
 """NOTES:
-
+- replace math for milliseconds
 """
 
 #imports
@@ -7,52 +7,48 @@ from machine import Pin
 import time
 
 #variables & pins
+infrared_sensor = Pin(15, Pin.IN)
+
 cycle = 0
 change_count = 0
 sections = 3
-infrared_sensor = Pin(15, Pin.IN)
+last_value_infrared = infrared_sensor.value()
+times = [0, 0, 0]
 
 #functions
-def changed(x, n):
-    n = n%2
-    values = []
-    values[n] = x
-    average = (values[0] + values[1])/2
-    if average != values[n]:
+def changed():
+    global last_value_infrared
+    current_value = infrared_sensor.value()
+    if last_value_infrared != current_value:
+        last_value_infrared = current_value
         return True
     else:
         return False
 
 def change_counter():
     global change_count
-    if changed(infrared_sensor.value(), cycle):
+    if changed():
         change_count += 1
-        return change_count
+        return change_count % 6
     else:
         return change_count
 
-def rotational_calc(times):
-    differences = [times[4] - times[2],
-                   times[2] - times[0],
-                   times[0] - times[4]]
-    differences[0] = 60 / (differences[0] * 3)
-    differences[1] = 60 / (differences[1] * 3)
-    differences[2] = 60 / (differences[2] * 3)
+def revolution_calc():
+    global times
+    differences = []
+
+
+    differences[0] = 60 / (differences[0] * sections)
+    differences[1] = 60 / (differences[1] * sections)
+    differences[2] = 60 / (differences[2] * sections)
     average_segment_time = sum(differences)/3
     return average_segment_time
 
-def rpm(changes, run_cycle):
-    x = changes % 6
-    time_list = []
-
-    if changed(changes, run_cycle):
-        time_list[x] = time.time()
-        return time_list
-    else:
-        return time_list
+def rpm():
+    pass
 
 #main
 while True: #If you have another while true statement, put the following code there instead
-    current_rpm = rotational_calc(rpm(change_count, cycle))
+    current_rpm = None
     cycle += 1 #counts how many times program has been run
     #check for corruption
