eafa6dfd844c91002c620a683266a6e91515d504
welwell <elwell.wi@northeastern.edu>
2026-03-13 13:54:17 -0400

    further drafting, adding plans for servo motion

--- python/DA3/draft1.py
+++ python/DA3/draft1.py
@@ -9,8 +9,10 @@ photo_right_raw = machine.ADC(26)
 servo_out = PWM(Pin(0))
 servo_out.freq(50)
 
+current_section = 0
+
 #functions
-def scaling(photo_pin) -> int:
+def downstep(photo_pin) -> int:
     raw = photo_pin.read_16()
     return raw // 5000
 
@@ -20,6 +22,16 @@ def balance(left: int, right:int):
         return abs(diff), "L"
     else:
         return abs(diff), "R"
+
+def spinner():
+    pass
+    """
+    Idea: divide full circle of servo range into 14 sections (0:13)
+    -> each section is similarly subdivided
+    -> magnitude of the difference determines how many subsections are added
+        or taken away based off of L / R dominance
+    -> resets to be relative to current section
+    """
 
 #main
 while True:
