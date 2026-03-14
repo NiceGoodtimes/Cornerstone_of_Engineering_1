1c094b2eaead8e3d27f5e64553b40cdbcdf9aa0a
welwell <elwell.wi@northeastern.edu>
2026-03-14 16:57:53 -0400

    draft of servo functionality

--- python/DA3/draft1.py
+++ python/DA3/draft1.py
@@ -4,27 +4,45 @@ from machine import Pin, PWM
 import time
 
 #global variables
-photo_left_raw = machine.ADC(27)
+photo_left_raw = machine.ADC(27) #photoresistors quantify brightness
 photo_right_raw = machine.ADC(26)
 servo_out = PWM(Pin(0))
 servo_out.freq(50)
 
-current_section = 0
-
 #functions
-def downstep(photo_pin) -> int:
+"""Photoresistor functions"""
+def downstep(photo_pin): #scales down photoresistor output
     raw = photo_pin.read_16()
     return raw // 5000
 
-def balance(left: int, right:int):
+def balance(left: int, right:int): #determines which side receives more light
     diff = right - left
     if diff < 0:
-        return abs(diff), "L"
+        return [abs(diff), "L"]
+    elif diff > 0:
+        return [abs(diff), "R"]
     else:
-        return abs(diff), "R"
+        return [0, "C"]
 
-def spinner():
-    pass
+"""servo functions"""
+def set_servo(mode: int): #sets the mode of the servo
+    servo_out.duty_u16(mode)
+
+def spinner(x: list): #determines how to spin the servo
+    forward = 6553
+    stop = 4915
+    reverse = 3277
+
+    if x[2] == "R":
+        set_servo(forward)
+        time.sleep_ms(10)
+        set_servo(stop)
+    elif x[2] == "L":
+        set_servo(reverse)
+        time.sleep_ms(10)
+        set_servo(stop)
+    else:
+        pass
     """
     Idea: divide full circle of servo range into 14 sections (0:13)
     -> each section is similarly subdivided
@@ -35,4 +53,6 @@ def spinner():
 
 #main
 while True:
+    left_real = downstep(photo_left_raw)
+    right_real = downstep(photo_right_raw)
     pass
