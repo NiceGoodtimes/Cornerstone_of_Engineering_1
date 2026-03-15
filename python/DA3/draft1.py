e85f4b3cb4993ea19cf6e79177592e024bd30e82
welwell <elwell.wi@northeastern.edu>
2026-03-15 16:07:13 -0400

    functional final version

--- python/DA3/draft1.py
+++ python/DA3/draft1.py
@@ -4,14 +4,15 @@ from machine import Pin, PWM
 import time
 
 #global variables
-photo_left_pin = machine.ADC(27) #photoresistors quantify brightness
-photo_right_pin = machine.ADC(26)
+photo_left_pin = machine.ADC(26) #photoresistors quantify brightness
+photo_right_pin = machine.ADC(27)
 servo_out = PWM(Pin(0))
 servo_out.freq(50)
 
 #functions
 """Photoresistor functions"""
 def downstep(photo_pin): #scales down photoresistor output
+    #print(photo_pin)
     return photo_pin // 5000
 
 def balance(left: int, right:int): #determines which side receives more light
@@ -34,21 +35,23 @@ def spinner(x): #determines how to spin the servo
 
     if x == "R":
         set_servo(forward)
-        time.sleep_ms(10)
+        time.sleep_ms(20)
         set_servo(stop)
     elif x == "L":
         set_servo(reverse)
-        time.sleep_ms(10)
+        time.sleep_ms(20)
         set_servo(stop)
     else:
         pass
 
 #main
 while True:
+    time.sleep(0.25)
     left_raw = photo_left_pin.read_u16()
     right_raw = photo_right_pin.read_u16()
 
     left_real = downstep(left_raw)
     right_real = downstep(right_raw)
 
+    print(left_real, right_real)
     spinner(balance(left_real, right_real))
