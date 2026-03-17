10edfe922f354c1362fdcf7a98b16938ebdaf86a
welwell <elwell.wi@northeastern.edu>
2026-03-17 14:26:22 -0400

    further testing for future projects

--- python/Testing.py
+++ python/Testing.py
@@ -1,8 +1,18 @@
-def balance(left: int, right:int):
-    diff = right - left
-    if diff < 0:
-        return abs(diff), "L"
-    else:
-        return abs(diff), "R"
+from random import randint
 
-print(balance(1000, 500))
+left = 0
+
+current_efficiency = 0
+potential_efficiency = 100
+
+def pretend_light():
+    global left, current_efficiency, potential_efficiency
+    left = randint(0, 30)
+
+def randnum():
+    global left, current_efficiency, potential_efficiency
+    percentage = ((left + left)/2)*randint(current_efficiency, potential_efficiency)
+    return f"Efficiency = {percentage}%"
+
+while True:
+    print(randnum())
