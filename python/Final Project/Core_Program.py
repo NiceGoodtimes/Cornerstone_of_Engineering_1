f6cfb70ed971076ef5258e7e7c7e9e7b170ba046
welwell <elwell.wi@northeastern.edu>
2026-04-06 12:36:19 -0400

    Programs for lcd display updated to work with custom data, to be added later

--- python/Final Project/Core_Program.py
+++ python/Final Project/Core_Program.py
@@ -4,14 +4,58 @@ EQUATIONS
 
 NOTES
     - Voltage will be 1/10 of actual, coming through divider
-    - two volatge sensors and resistor,
-    meauring difference over the sensors,
+    - two voltage sensors and resistor,
+    measuring difference over the sensors,
 
     (v1-v2)/r = i
 """
 
-# Imports
+"""
+IMPORTS
+"""
+from machine import I2C, Pin
+from pico_i2c_lcd import I2cLcd
+from time import sleep
 
-# Variables
+"""
+VARIABLES
+"""
+typing_delay = 0.1
+text_output = "No Text Provided"
+lcd_data = 0
+lcd_clock = 1
 
-# Functions
+"""
+PIN ASSIGNMENTS
+"""
+i2c = I2C(0, sda=Pin(lcd_data), scl=Pin(lcd_clock), freq=400000)   #Data pin 0, clock pin 1
+I2C_ADDR = i2c.scan()[0]
+lcd = I2cLcd(i2c, I2C_ADDR, 2, 16)
+
+"""
+FUNCTIONS
+"""
+#displays each character incrementally on the screen
+def text_printer(display, text):
+    global typing_delay
+    for i in text:
+        display.putstr(i)
+        sleep(typing_delay)
+
+"""
+MAIN
+"""
+while True:
+    #cursor startup
+    lcd.show_cursor()
+    lcd.blink_cursor_on()
+    #display desired text
+    text_printer(lcd, text_output)
+    #pause
+    sleep(3)
+    #cursor end
+    lcd.blink_cursor_off()
+    lcd.hide_cursor()
+    #reset the screen (refresh rate)
+    sleep(1)
+    lcd.clear()
---
+++ python/Testing.py
@@ -0,0 +1,20 @@
+"""
+a^2 + b^2 = c^2
+find pythagorean triples
+"""
+from math import sqrt
+
+def pythagorean(a: int, b: int):
+    c = sqrt(a^2 + b^2)
+
+    if c != c//1:
+        pass
+    else:
+        return int(c)
+
+for i in range(1, 100):
+    for j in range(1, 100):
+        if pythagorean(i, j) is None:
+            continue
+        else:
+            print(pythagorean(i, j))
